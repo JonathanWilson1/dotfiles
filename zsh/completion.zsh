203139331148b07b6c616ecc495dd199e67e95bb
Jonathan Wilson <5080673+JonathanWilson1@users.noreply.github.com>
2022-08-23 22:44:01 +0100

    Missing files

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -3,3 +3,6 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
+
+# Error when terminal launched - https://stackoverflow.com/a/63447799
+for f in $(compaudit);do sudo chmod -R 755 $f;done;
---
+++ zsh/instal.sh
@@ -0,0 +1,2 @@
+# Dependancy of the Spacehip theme - https://github.com/powerline/fonts
+sudo apt-get install fonts-powerline
